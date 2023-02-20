2b325ab07705e553ea9a73da4f7b3f9f33331fcd
4yvakZ <35529078+4yvakZ@users.noreply.github.com>
2023-02-20 12:27:04 +0300

    TestObject small changes

--- DirectX11-Game-Framework/TestGameObject.cpp
+++ DirectX11-Game-Framework/TestGameObject.cpp
@@ -24,17 +24,17 @@ renderComponent->AddIndex(0);
 renderComponent->AddIndex(1);
 renderComponent->AddIndex(2);*/
 
-	/*renderComponent->AddTriangle(Vector4(0.25f, 0.7f, 0.5f, 1.0f),
+	renderComponent->AddTriangle(Vector4(0.25f, 0.7f, 0.5f, 1.0f),
 		Vector4(0.5f, 1.0f, 0.5f, 1.0f),
-		Vector4(0.8f, 0.3f, 0.5f, 1.0f));*/
+		Vector4(0.8f, 0.3f, 0.5f, 1.0f));
 
 	DirectX::SimpleMath::Rectangle r(-100, 400, 200, 800);
 	renderComponent->Add2DRect(r, Color(1, 0, 0, 1));
 
-	/*for (int i = 64; i > 2; i /= 2) {
+	for (int i = 64; i > 2; i /= 2) {
 		renderComponent->Add2DCircle(Vector4(), 100, i, Color(rand() % 10 / 10.f, rand() % 10 / 10.f, rand() % 10 / 10.f));
 	}
-	renderComponent->Add2DCircle(Vector4(-300, -300, 0, 1), 20);*/
+	renderComponent->Add2DCircle(Vector4(-300, -300, 0, 1), 20);
 
 	GameObject::Initialize();
 }
